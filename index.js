7661a007f67b8d57471ef407b62f3413af39590d
Renardo Williams <rennwill@gmail.com>
2023-04-03 15:23:53 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+const newHeader = document.createElement("h1")
+main.remove();
+
+newHeader.id ='victory';
+newHeader.textContent = "Renardo is the champion"
+
+document.body.append(newHeader);
